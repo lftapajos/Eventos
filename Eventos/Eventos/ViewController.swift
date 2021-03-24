6583655fffe129da8cc0e3c9d1adf1869d09e685
Luis Felipe Tapajos <lftapajos@gmail.com>
2021-03-24 01:52:30 -0300

    criando projeto de Eventos

---
+++ Eventos/Eventos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eventos
+//
+//  Created by Luis Felipe Tapajos on 24/03/2021.
+//  Copyright © 2021 Luis Felipe Tapajos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
